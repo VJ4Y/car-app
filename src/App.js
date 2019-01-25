a1d1e8f83b1d5f472de24ed1d0e7b07831bca42a
VJ4Y <31595765+VJ4Y@users.noreply.github.com>
2019-01-25 17:50:18 +0000

    Adding ga tracker

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React, {Component} from 'react';
+import ReactGA from 'react-ga';
 import logo from './logo.svg';
 import './App.css';
 import CarList from './components/CarList.js';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
+
+function initializeReactGA() {
+ ReactGA.initialize('UA-133103090-1');
+ ReactGA.pageview('/homepage');
+} 
 
 class App extends Component {
   render() {

34e0dd76f5f510fe86d2a95586e884978f1087f8
VJ@Y <vjrnjn@gmail.com>
2019-01-26 01:05:03 +0530

    react-ga install

--- src/App.js
+++ src/App.js
@@ -7,12 +7,14 @@ import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 
 function initializeReactGA() {
+  console.log('in reactga '+window.location.pathname)
  ReactGA.initialize('UA-133103090-1');
- ReactGA.pageview('/');
+ ReactGA.pageview(window.location.pathname);
 } 
 
 class App extends Component {
   render() {
+    initializeReactGA();
     return (
       <div className="App">
       <AppBar position="static" style={{backgroundColor:"#282c34"}}>
